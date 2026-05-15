6c50e7be6a4cbfb0a95eb389412b475d9471e614
michele.mazzini <michele.mazzini1996@gmail.com>
2026-05-15 17:50:39 +0200

    fix: fix log

--- src/common/envVars.js
+++ src/common/envVars.js
@@ -114,7 +114,7 @@ export function buildDefaultParams(apiName, token) {
   const parentId = uuidv4().replace(/-/g, '').substring(0, 16);
   const traceparent = `00-${GLOBAL_TRACE_ID}-${parentId}-01`;
 
-  console.log(`[TRACE INFO] API: ${apiName} | TRACE_ID: ${currentTraceId}`);
+  console.log(`[TRACE INFO] API: ${apiName} | TRACE_ID: ${GLOBAL_TRACE_ID} | traceparent: ${traceparent}`);
 
   return {
     headers: Object.assign(
